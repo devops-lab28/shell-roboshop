db0c138a8342e12398ebdcaba7e9cfe28417a8d9
Nikhilesh Sandela <nikhileshsandela28@gmail.com>
2026-04-08 21:32:57 +0200

    actions

---
+++ roboshop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SG_ID="sg-05ef51454ebbf03b8"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+echo "Launching EC2 instance..."
+
+for instance in $@
+do
+    # echo "$instance"
+    # gives the private IP address of the launched instance
+    instance_id=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].PrivateIpAddress' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    fi
+done
+
+# 
