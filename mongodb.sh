e41628ccaef01543fe7712949b467745ff3129de
Nikhilesh Sandela <nikhileshsandela28@gmail.com>
2026-04-08 22:31:41 +0200

    actions

---
+++ mongodb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGS_FILE
+VALIDATE $? "Copying MongoDB repo file"
+
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod &>> $LOGS_FILE
+VALIDATE $? "Enabling MongoDB service"
+
+systemctl start mongod &>> $LOGS_FILE
+VALIDATE $? "Starting MongoDB service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGS_FILE
+VALIDATE $? "Allowing MongoDB to listen on all interfaces (allowing remote connections)"
+
+systemctl restart mongod &>> $LOGS_FILE
+VALIDATE $? "Restarting MongoDB service"
